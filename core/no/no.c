b3d63fe2eaf23d667795864949458e0e50dc44f7
Rebecca White <rtw@null.net>
2021-12-13 14:59:35 +0000

    'no' and Makefile Changes
    --> No
    No is the replacement for the GNU yes program.
    
    --> Makefile
    Main makefile now tells the user the options, and each individual
    makefile also has the 'make install' and 'make uninstall' options

---
+++ core/no/no.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+	if (argc == 1)
+	{
+		displayHelp(argc, argv);
+		exit(0);
+	} else {
+		output(argc, argv);
+	}
+}
+
+int output(int argc, char *argv[])
+{
+	int i = 1;
+	while (true)
+	{
+		for (i=1; i<argc; i++)
+		{
+			printf("%s ",argv[i]);
+		}
+		printf("\n");
+	}
+}
+
+int displayHelp(int argc, char *argv[])
+{
+	printf("No - A Replacement for the 'yes' Utility\n");
+	printf("version 0.1\n");
+	printf("Usage: %s [STRING]\n", argv[0]);
+}
